d07086d72e6884a31b5539090057b8e5c64cd138
Daniël Franke <daniel.franke@cloud.ionos.com>
2020-11-20 19:38:27 +0100

    Add start of install script.

---
+++ .local/bin/install.sh
@@ -0,0 +1,262 @@
+#!/usr/bin/env bash
+
+OS_RELEASE="/etc/os-release"
+SUDO="/usr/bin/sudo"
+GUI=false
+
+export PATH="$PATH:$HOME/go/bin:$HOME/.local/bin:$HOME/.cargo/bin"
+
+info() {
+    echo -e "\e[32mI:\e[0m $@"
+}
+
+error() {
+    echo -e "\e[31mE:\e[0m $@"
+}
+
+fatal() {
+    echo -e "\e[91mF:\e[0m $@"
+    exit 1
+}
+
+fatal_on_fail() {
+    (($? != 0)) && fatal $@
+}
+
+print_usage() {
+    echo "$0 - Sets up an Ubuntu/Arch machine according to ainmosni's spec"
+    echo ""
+    echo "usage: $0"
+    echo "usage: $0 -g"
+    echo ""
+    echo "Options:"
+    echo "  -g\t\tSet up GUI."
+    echo "  -h\t\tThis help text."
+    echo ""
+    exit 1
+}
+
+detect_os()
+{
+    info "Detecting OS..."
+
+    if [ -f "$OS_RELEASE" ]; then
+        . $OS_RELEASE
+    else
+        fatal "Unknown OS, existing."
+    fi
+
+    info "Detected OS: $NAME"
+}
+
+cache_sudo_password() {
+    if [ ! -x "$SUDO" ]; then
+        fatal "sudo not found or not executable."
+    fi
+    info "Initialising sudo password"
+    $SUDO /usr/bin/true
+    fatal_on_fail "sudo failed"
+}
+
+install_dependencies()
+{
+    case $ID in
+        "ubuntu")
+            install_ubuntu_dependencies
+            (($GUI == "true")) && install_ubuntu_gui_dependencies
+            ;;
+        "arch")
+            install_arch_dependencies
+            (($GUI == "true")) && install_arch_gui_dependencies
+            ;;
+        *)
+            fatal "Unsupported OS: $NAME"
+            ;;
+    esac
+
+    install_rust
+    install_precommit
+    install_rust_dependencies
+}
+
+install_rust()
+{
+    info "Installing rust via rustup."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+}
+
+install_precommit()
+{
+    curl https://pre-commit.com/install-local.py | python3 -
+}
+
+install_ubuntu_dependencies()
+{
+    sudo apt update
+    sudo apt install -y apt-transport-https gnupg2 curl
+
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+    echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+    sudo add-apt-repository -y ppa:longsleep/golang-backports
+    curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+
+    sudo apt update
+    sudo apt upgrade -y
+    sudo apt install -y \
+        fish \
+        fzf \
+        git \
+        golang-go \
+        httpie \
+        jq \
+        kubectl \
+        libssl-dev \
+        neovim \
+        nodejs \
+        pcscd \
+        python3-dev \
+        python3-pip \
+        python3-setuptools \
+        ripgrep \
+        scdaemon
+    sudo pip3 install thefuck
+}
+
+install_ubuntu_gui_dependencies()
+{
+    echo "deb https://apt.enpass.io/ stable main" | sudo tee -a /etc/apt/sources.list.d/enpass.list
+    curl -s https://apt.enpass.io/keys/enpass-linux.key | sudo apt-key add -
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
+    echo "deb http://apt.insync.io/${ID} ${VERSION_CODENAME} non-free contrib" | sudo tee -a /etc/apt/sources.list.d/insync.list
+
+    sudo apt update
+
+    # Missing packages for Ubuntu:
+    #   rofi-pinentry
+    sudo apt install -y \
+        enpass \
+        firefox \
+        fonts-firacode \
+        grim \
+        insync \
+        libwayland-dev \
+        libx11-dev \
+        libxcursor-dev \
+        libxmu-dev \
+        libxmu-headers \
+        libxpm-dev \
+        light \
+        mako-notifier \
+        neovim-qt \
+        playerctl \
+        rofi \
+        sway \
+        swaybg \
+        swayidle \
+        swaylock \
+        waybar \
+        xbitmaps 
+    sudo snap install --candidate  termite
+}
+
+get_yay()
+{
+    curdir=$(pwd)
+    yaydir=$(mktemp -d)
+    cd $yaydir
+    curl -L https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz | tar xzvf -
+    cd yay
+    makepkg -si
+    pacman -U yay-*.tar.zst
+    cd $curdir
+}
+
+install_arch_dependencies()
+{
+    sudo pacman -Syu
+    sudo pacman -S base-devel curl
+    get_yay
+    yay -S \
+        ccid \
+        fish \
+        fzf \
+        git \
+        go \
+        httpie \
+        jq \
+        kubectl \
+        kubectx \
+        neovim \
+        nodejs \
+        pcsclite \
+        python \
+        ripgrep \
+        thefuck
+}
+
+install_arch_gui_dependencies()
+{
+    yay -S \
+        enpass-bin \
+        firefox \
+        gdm \
+        grim \
+        insync \
+        light \
+        mako \
+        neovim-qt  \
+        otf-font-awesome \
+        otf-font-awesome-4 \
+        pinentry-rofi \
+        playerctl \
+        rofi \
+        sway \
+        swaybg \
+        swayidle \
+        swaylock \
+        ttf-dejavu \
+        ttf-fira-code \
+        ttf-fira-sans \
+        ttf-font-awesome \
+        ttf-font-awesome-4 \
+        ttf-joypixels \
+        ttf-liberation \
+        ttf-linux-libertine \
+        ttf-noto-sans-kannada \
+        ttf-symbola \
+        ttf-twemoji \
+        waybar
+}
+
+install_rust_dependencies()
+{
+    cargo install \
+        exa \
+        bat \
+        starship \
+        dust 
+}
+
+install_go_dependencies()
+{
+    go get -u github.com/xyproto/wallutils/cmd/setrandom
+    go get -u github.com/r00tman/corrupter
+    go get -u github.com/ahmetb/kubectx/cmd/kubens
+    go get -u github.com/ahmetb/kubectx/cmd/kubectx
+    go get -u github.com/ainmosni/golang-gmail-check
+    go get -u gihub.com/ainmosni/wayweather
+}
+
+
+while getopts 'gh' flag; do
+    case "${flag}" in
+        g) GUI=true ;;
+        *) print_usage ;;
+    esac
+done
+
+echo $GUI
+
+detect_os
+cache_sudo_password
+install_dependencies
